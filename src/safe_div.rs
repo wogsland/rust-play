37245834b0c1ca1f8a04715868e80b5c50dace0e
Bradley Wogsland <bradley@wogsland.org>
2017-01-17 15:37:15 -0600

    all good

--- src/safe_div.rs
+++ src/safe_div.rs
@@ -1,10 +1,5 @@
 use std::io;
 
-/*enum Option<T> {
-    None,
-    Some(T)
-}*/
-
 fn safe_div(n: i32, d: i32) -> Option<i32> {
     if d == 0 {
         return None;
@@ -13,17 +8,16 @@ fn safe_div(n: i32, d: i32) -> Option<i32> {
 }
 
 fn main() {
-    println!("Guess the number!");
     println!("Please input your numerator.");
     let mut numerator = String::new();
     io::stdin().read_line(&mut numerator)
         .expect("Failed to read line");
-    let numerator_int = numerator.parse::<i32>().unwrap();
+    let numerator_int = numerator.trim().parse::<i32>().unwrap();
     println!("Please input your denominator.");
     let mut denominator = String::new();
     io::stdin().read_line(&mut denominator)
         .expect("Failed to read line");
-    let denominator_int = denominator.parse::<i32>().unwrap();
+    let denominator_int = denominator.trim().parse::<i32>().unwrap();
     match safe_div(numerator_int, denominator_int) {
         None => println!("Can't divide by zero!"),
         Some(v) => println!("Quotient is {}", v)
